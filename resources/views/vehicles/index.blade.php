73394c9629bf66c5307d486dfbdc9d53f212fe91
benlittlenz <ben@pop-os.localdomain>
2020-10-10 11:48:19 +1300

    added blade components

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -1,15 +1,14 @@
 <x-app-layout>
-    <x-slot name="header">
+<x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Vehicles') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <!-- <x-jet-welcome /> -->
-                <livewire:vehicle-table />
+                <livewire:vehicle-table :vehicles="$vehicles"/>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Vehicle;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function() {
+
+    Route::get('/vehicles', \App\Http\Livewire\VehicleTable::class);
+
+
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+});
