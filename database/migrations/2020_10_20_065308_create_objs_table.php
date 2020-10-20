bee95fd83f9856769b7389411b1b2a392f27f177
benlittlenz <ben@pop-os.localdomain>
2020-10-20 19:57:16 +1300

    added objects migration structure

---
+++ database/migrations/2020_10_20_065308_create_objs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateObjsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('objects', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->morphs('objectable');
+            $table->foreignId('parent_id')->nullable()->constrained('objects');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('objs');
+    }
+}
