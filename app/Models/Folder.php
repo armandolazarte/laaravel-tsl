109728392848fe9c110afad95fd201133364808b
benlittlenz <ben@pop-os.localdomain>
2020-10-20 20:00:52 +1300

    added file and folders migrations

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Obj extends Model
+class Folder extends Model
 {
     use HasFactory;
-
-    public $table = 'objects';
 }
