9257a77372b8e2cacf27340cfb7113c0ec654be9
benlittlenz <ben@pop-os.localdomain>
2020-10-24 13:47:21 +1300

    moved file html to livewire template file

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Obj;
 use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
-    public function index()
+
+    public function __construct()
     {
-        return view('files');
+        $this->middleware(['auth']);
+    }
+
+    public function index(Request $request)
+    {
+        $user_id = $request->user()->company->id;
+
+        $object = Obj::where('company_id', $user_id)->where(
+            'uuid', $request->get('uuid', Obj::where('company_id', $user_id)
+            ->whereNull('parent_id')->first()->uuid))
+            ->firstOrFail();
+
+        dd($object);
+
+        return view('files', [
+            'object' => $object
+        ]);
     }
 }
