b4e0b39409e3f5211f1da043f90eda4903418e40
benlittlenz <ben@pop-os.localdomain>
2020-11-14 11:21:22 +1300

    added livewire component with table setup

---
+++ app/Http/Livewire/Suppliers.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Supplier;
+use Livewire\WithPagination;
+use App\Http\Livewire\DataTable\WithSorting;
+use App\Http\Livewire\DataTable\WithBulkActions;
+use App\Http\Livewire\DataTable\WithPerPagePagination;
+
+class Suppliers extends Component
+{
+    use WithPerPagePagination, WithPagination, WithSorting, WithBulkActions;
+
+    public $search = '';
+    public $showNonApproved = true;
+    public $showDeleteModal = false;
+    public $showApproveModal = false;
+
+    public $showEditModal = false;
+    public $showFilters = false;
+    public $editing;
+    public $successMessage;
+
+    //protected $queryString = ['sortField', 'sortDirection'];
+
+    protected $listeners = ['refreshVehicles' => '$refresh'];
+
+    protected $rules = [
+        'editing.started_at' => 'required',
+        'editing.stopped_at' => 'required',
+        'editing.morning_break' => 'required',
+        'editing.afternoon_break' => 'required',
+        'editing.comments' => 'required',
+    ];
+
+    public function updatedFilters() { $this->resetPage(); }
+
+    public function exportSelected()
+    {
+        return response()->streamDownload(function () {
+            echo $this->selectedRowsQuery->toCsv();
+        }, 'staff.csv');
+    }
+
+    public function deleteSelected()
+    {
+        $deleteCount = $this->selectedRowsQuery->delete();
+
+        $this->showDeleteModal = false;
+
+        $this->dispatchBrowserEvent('notify', 'You\'ve deleted '.$deleteCount.' suppliers');
+    }
+
+
+    public function create()
+    {
+        $this->editing = Supplier::make([]);
+        $this->showEditModal = true;
+    }
+
+
+    public function edit($supplierID)
+    {
+        $supplier = Supplier::find($supplierID);
+
+        $this->editing = $supplier;
+
+        $this->showEditModal = true;
+    }
+
+    public function save()
+    {
+        $this->validate();
+        $this->editing->save();
+
+        $this->successMessage =  'Comment posted!';
+
+        $this->showEditModal = false;
+
+        $this->dispatchBrowserEvent('notify', 'Supplier updated!');
+    }
+
+    public function getRowsQueryProperty()
+    {
+        $query =  Supplier::query()
+            ->search($this->search);
+        return $this->applySorting($query);
+    }
+
+    public function getRowsProperty()
+    {
+        return $this->applyPagination($this->rowsQuery);
+    }
+
+    public function render()
+    {
+        if ($this->selectAll) {
+            $this->selectPageRows();
+        }
+
+        return view('livewire.suppliers', [
+            'timesheets' => $this->rows
+        ]);
+    }
+}
--- database/factories/SupplierFactory.php
+++ database/factories/SupplierFactory.php
@@ -22,7 +22,14 @@ class SupplierFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'company_name' => $this->faker->company,
+            'account_number' => $this->faker->bankAccountNumber,
+            'email' => $this->faker->safeEmail,
+            'contact_name' => $this->faker->name,
+            'contact_number' => $this->faker->phoneNumber,
+            'street_address' => $this->faker->streetAddress,
+            'suburb' => $this->faker->state,
+            'city' => $this->faker->city,
         ];
     }
 }
