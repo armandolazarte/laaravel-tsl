52cdc0162c6541b979f5cda4fdbe012deaba13be
benlittlenz <ben@pop-os.localdomain>
2020-11-14 11:23:12 +1300

    added search scope to supplier model

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -10,4 +10,20 @@ class Supplier extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function scopeSearch($query, string $terms = null)
+    {
+        collect(explode(' ', $terms))->filter()->each(function ($term) use ($query) {
+            $term = '%'.$term.'%';
+
+
+            $query->where(function ($query) use ($term) {
+                $query->where('company_name', 'like', $term)
+                ->orWhere('account_number', 'like', $term)
+                ->orWhere('email', 'like', $term)
+                ->orWhere('contact_name', 'like', $term)
+                ->orWhere('street_address', 'like', $term);
+            });
+        });
+    }
 }
